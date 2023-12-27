7aa80fa67eb36edfeca403d79c0d514caf6feb8a
aryamanchandra <aryamanhere@gmail.com>
2023-12-28 03:31:04 +0530

    message input placeholder color

--- packages/client/src/components/Chat.js
+++ packages/client/src/components/Chat.js
@@ -1,5 +1,7 @@
 import {
   Divider,
+  Grid,
+  GridItem,
   HStack,
   Input,
   TabPanel,
@@ -11,48 +13,67 @@ import React from "react";
 
 function Chat() {
   return (
-    <VStack p="5" px="10">
-      <TabPanels>
-        <TabPanel>
-          <HStack h="5vh">
-            <Text m="0" color="blue.800">
-              Varun
-            </Text>
-            <Divider orientation="vertical" borderColor="gray.500" />
-            <Text fontSize="sm" color="gray.500" m="0">
-              Online
-            </Text>
-          </HStack>
-        </TabPanel>
-        <TabPanel>
-          <HStack h="5vh">
-            <Text m="0" color="blue.800">
-              Shashwat
-            </Text>
-            <Divider orientation="vertical" borderColor="gray.500" />
-            <Text fontSize="sm" color="gray.500" m="0">
-              Online
-            </Text>
-          </HStack>
-        </TabPanel>
-        <TabPanel>
-          <HStack h="5vh">
-            <Text m="0" color="blue.800">
-              Ashvin
-            </Text>
-            <Divider orientation="vertical" borderColor="gray.500" />
-            <Text fontSize="sm" color="gray.500" m="0">
-              Online
-            </Text>
-          </HStack>
-        </TabPanel>
-      </TabPanels>
-      <Input
-        placeholder="Basic usage"
-        color="blue.500"
-        marginX="20"
-        sx={{ border: "2px solid #CBD5E0", marginTop: "auto", marginX: "50px !important" }}
-      />
+    <VStack p="5" px="10" height="100%">
+      <Grid width="full" height="100%" templateRows="repeat(12, 1fr)">
+        <GridItem rowSpan={2}>
+          <TabPanels>
+            <TabPanel>
+              <HStack h="4vh">
+                <Text m="0" color="blue.800">
+                  Varun
+                </Text>
+                <Divider orientation="vertical" borderColor="gray.500" />
+                <Text fontSize="sm" color="gray.500" m="0">
+                  Online
+                </Text>
+              </HStack>
+            </TabPanel>
+            <TabPanel>
+              <HStack h="5vh">
+                <Text m="0" color="blue.800">
+                  Shashwat
+                </Text>
+                <Divider orientation="vertical" borderColor="gray.500" />
+                <Text fontSize="sm" color="gray.500" m="0">
+                  Online
+                </Text>
+              </HStack>
+            </TabPanel>
+            <TabPanel>
+              <HStack h="5vh">
+                <Text m="0" color="blue.800">
+                  Ashvin
+                </Text>
+                <Divider orientation="vertical" borderColor="gray.500" />
+                <Text fontSize="sm" color="gray.500" m="0">
+                  Online
+                </Text>
+              </HStack>
+            </TabPanel>
+          </TabPanels>
+        </GridItem>
+        <GridItem rowSpan={10}>
+          <Text>Hello this is some text</Text>
+        </GridItem>
+        <GridItem rowSpan={2}>
+          <Input
+            placeholder="Type a message"
+            variant="filled"
+            type="text"
+            color="gray.500"
+            marginTop="auto"
+            marginBottom="20px"
+            sx={{
+              border: "2px solid #CBD5E0",
+              marginTop: "auto",
+              _placeholder: { color: "gray.500" },
+              "&:hover": {
+                border: "2px solid #A0AEC0",
+              },
+            }}
+          />
+        </GridItem>
+      </Grid>
     </VStack>
   );
 }
