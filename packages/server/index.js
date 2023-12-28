732c81143cb1bcfa4375b69c976f9fcc04429284
aryamanchandra <aryamanhere@gmail.com>
2023-12-28 23:56:07 +0530

    backend, redis, session

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,10 +1,47 @@
 const express = require("express");
-const app =  express();
+const { Server } = require("socket.io");
+const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const server = require("http").createServer(app);
+require("dotenv").config();
+const Redis = require("ioredis");
+const RedisStore = require("connect-redis")(session);
 
-app.get("/", (req,res) => {
-    res.send("Hello world");
-})
+const io = new Server(server, {
+  cors: {
+    origin: "https://localhost:3000",
+    credentials: "true",
+  },
+});
 
-app.listen(5000, () => {
-    console.log("hello")
-})
+app.use(helmet());
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    credentials: true,
+    name: "aryaman",
+    store: new RedisStore({ client: redisClient }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.ENVIRONMENT === "production" ? "true" : "auto",
+      httpOnly: true,
+      expires: 1000 * 60 * 60 * 24 * 7,
+      sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+io.on("connect", (socket) => {});
+
+server.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
