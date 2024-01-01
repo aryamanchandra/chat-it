b883782808ecfee25c0183cffd55112d400a3ccf
aryamanchandra <aryamanhere@gmail.com>
2024-01-01 23:27:17 +0530

    socketSetup

--- packages/client/src/components/socketSetup.js
+++ packages/client/src/components/socketSetup.js
@@ -1,16 +1,16 @@
-// import { useContext, useEffect } from "react";
-// import socket from "../../socket";
+import { useContext, useEffect } from "react";
+import socket from "../../socket";
 
-// const useSocketSetup = () => {
-//   useEffect(() => {
-//     socket.connect();
-//     socket.on("connect_error", () => {
-//       setUser({ loggedIn: false });
-//     });
-//     return () => {
-//       socket.off("connect_error");
-//     };
-//   }, [setUser]);
-// };
+const useSocketSetup = () => {
+  useEffect(() => {
+    socket.connect();
+    socket.on("connect_error", () => {
+      setUser({ loggedIn: false });
+    });
+    return () => {
+      socket.off("connect_error");
+    };
+  }, [setUser]);
+};
 
-// export default useSocketSetup;
+export default useSocketSetup;
